c65e48b15e4b1be41337ad5859b68f22d1fd6663
Roland <rmargve@gmail.com>
2022-10-25 00:25:39 -0700

    initial commit

---
+++ src/dumpRecipes.js
@@ -0,0 +1,30 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+async function bootstrap() {
+  let page = 1;
+  const limit = 20;
+
+  let url = `${process.env.API_RECIPE_LIST_URL}&page=${page}&limit=${limit}`;
+  let response = await fetch(url);
+  let data = await response.json();
+
+  const total = data.total;
+  const recipes = data.rows;
+
+  while (page * limit < total) {
+    page++;
+    console.log(`fetching page ${page}, total ${Math.ceil(total / limit)}`);
+    url = `${process.env.API_RECIPE_LIST_URL}&page=${page}&limit=${limit}`;
+    response = await fetch(url);
+    data = await response.json();
+    recipes.push(...data.rows);
+
+  }
+
+  fs.writeFileSync(`./data/recipes.json`, JSON.stringify(recipes), { enconding: 'utf8' });
+}
+
+bootstrap();
